0b198f479728be566fc32f24231d9291f1685847
Zain <zainarif00456@gmail.com>
2022-11-15 16:44:39 +0500

    2nd Commit with a little modification in backend

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/", methods=["GET"])
 def index():
+    """
+    for Testing the working of API
+    :return:
+    """
     return "Flask API for Blood Donation Application"
 
 
+@app.route("/register", methods=["POST"])
+def registration():
+    payload = request.get_json()
+    print(payload)
+    return {"details": "Registration Successful"}
+
+
 if __name__ == '__main__':
-    app.run("0.0.0.0", debug=True)
+    app.run("0.0.0.0", debug=True)  # Running on all IP addresses.
